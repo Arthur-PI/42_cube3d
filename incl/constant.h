93c95aacb0a1956aa13128a57120a851adb5efc5
Arthur Pigeon <arthur.pigeon@harfanglab.fr>
2023-01-09 11:28:50 +0100

    feat: configure loop hook and default closing option for the window

---
+++ incl/constant.h
@@ -0,0 +1,37 @@
+#ifndef CONSTANT_H
+# define CONSTANT_H
+
+# define WIN_WIDTH 1000
+# define WIN_HEIGHT 500
+# define WIN_TITLE "Cube3D"
+
+# define ON_DESTROY 17
+# define ON_KEYDOWN 2
+
+# define KEY_ESC 65307
+# define KEY_W 119
+# define KEY_S 115
+# define KEY_A 97
+# define KEY_D 100
+# define KEY_O 111
+# define KEY_P 112
+# define KEY_K 107
+# define KEY_L 108
+# define KEY_N 110
+# define KEY_M 109
+# define KEY_1 49
+# define KEY_2 50
+# define KEY_PLUS 61
+# define KEY_MINUS 45
+# define KEY_ARROW_UP 65362
+# define KEY_ARROW_DOWN 65364
+# define KEY_ARROW_LEFT 65361
+# define KEY_ARROW_RIGHT 65363
+
+# define MOUSE_SCROLL_UP 5
+# define MOUSE_SCROLL_DOWN 4
+
+# define MASK_NO_EVENT 0L
+# define MASK_KEY_PRESS 1L
+
+#endif
